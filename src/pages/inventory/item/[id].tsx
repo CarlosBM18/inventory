289222b3c9d813d962d54ccb98aa5ebf76964c05
Carlos Bertomeu <cbertomeu18@gmail.com>
2022-09-24 17:07:28 +0200

    Item page

---
+++ src/pages/inventory/item/[id].tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import React from "react"
+import { trpc } from "../../../utils/trpc"
+
+const ItemPageContent: React.FC<{ id: string }> = ({ id }) => {
+  const { data } = trpc.useQuery(['items.get-by-id', { id }])
+  const { mutate, isLoading } = trpc.useMutation('items.delete')
+
+  if (!data || !data?.name) {
+    return <div>Item not found</div>
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        {data.id}
+      </div>
+      <div>
+        {data.name}
+      </div>
+      <button type="button" disabled={isLoading} onClick={() => {
+        if (!isLoading) {
+          mutate({ id: data.id })
+        }
+      }} className="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+        {isLoading ? 'Deleting' : 'Delete'}
+      </button>
+      <Link href='/inventory'>
+        <a type="button" className="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+          Go back
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+const ItemPage = () => {
+  const { query } = useRouter()
+  const { id } = query
+
+  if (!id || typeof id !== "string") {
+    return <div>No ID</div>
+  }
+
+  return <ItemPageContent id={id} />
+}
+
+export default ItemPage
