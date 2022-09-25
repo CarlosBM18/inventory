d6cb6c042e0d4f4cd368719b2d8dcfa8133a2f34
Carlos Bertomeu <cbertomeu18@gmail.com>
2022-09-25 23:02:52 +0200

    Fix name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="font-bold text-2xl mb-4">Inventory Software</h1>
-        <h2 className="font-medium text-l mb-4">Scan, link, visualize and more...</h2>
+        <h1 className="font-bold text-2xl mb-4">IST Inventory</h1>
         <Link href='/inventory'>
           <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Go to Inventory</a>
         </Link>
